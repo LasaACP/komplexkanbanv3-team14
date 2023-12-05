e098d1407288e7f13fce19abaeaabe5daa3ad70d
stationarybird <william.z.gu@gmail.com>
2023-12-05 01:09:02 +0000

    added stub for addition

--- src/Complex.cpp
+++ src/Complex.cpp
@@ -1,14 +1,14 @@
-class Complex{
+class Complex {
 
-  public: 
-    double real;
-    double img;
-Complex(){
-  real = 0;
-  img = 0;
-}
-Complex(double r, double i){
-  real = r;
-  img = i;
-}
+public:
+  double real;
+  double img;
+  Complex() {
+    real = 0;
+    img = 0;
+  }
+  Complex(double r, double i) {
+    real = r;
+    img = i;
+  }
 };
