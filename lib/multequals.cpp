dbd8c0073bf34966971c7fd33bbd24ba78f59151
aahFancyGun <yangfanchau2008@gmail.com>
2023-12-06 15:43:49 +0000

    updating multequals method

--- lib/multequals.cpp
+++ lib/multequals.cpp
@@ -1,4 +1,6 @@
 #include "multequals.h"
+#include "multiplication.h"
+
 
 Complex operator *=(const Complex &z, const Complex &w){
   return Complex(z.real*w.real - z.img*w.img, z.real*w.img + z.img*w.real);
