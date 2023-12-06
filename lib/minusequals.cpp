e31889bc519437433d608633952c5d92e04c7382
ExamWeHaltLitMaTowel <liammaster2007@gmail.com>
2023-12-06 15:26:32 +0000

    minusequals method

---
+++ lib/minusequals.cpp
@@ -0,0 +1,12 @@
+#include "subtraction.h"
+#include "minusequals.h"
+
+Complex operator -= (const Complex &z, const Complex &w) {
+  return Complex(z.real - w.real, z.img - w.img);
+}
+Complex operator -= (const Complex &z, const double &w) {
+  return Complex(z.real - w, z.img);
+}
+Complex operator -= (const double &z, const Complex &w) {
+  return Complex(z - w.real, w.img);
+}
