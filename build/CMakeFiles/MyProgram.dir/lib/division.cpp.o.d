fcb97f52c2fe8f37f101c5029fdd6ac22545aae5
stationarybird <william.z.gu@gmail.com>
2023-12-06 19:48:17 +0000

    .o and .d files in github!

---
+++ build/CMakeFiles/MyProgram.dir/lib/division.cpp.o.d
@@ -0,0 +1,7 @@
+CMakeFiles/MyProgram.dir/lib/division.cpp.o: \
+ /home/runner/KomplexKanban-V3-WilliamGu3/lib/division.cpp \
+ /nix/store/4pqv2mwdn88h7xvsm7a5zplrd8sxzvw0-glibc-2.35-163-dev/include/stdc-predef.h \
+ /home/runner/KomplexKanban-V3-WilliamGu3/lib/Complex.h \
+ /home/runner/KomplexKanban-V3-WilliamGu3/lib/division.h \
+ /home/runner/KomplexKanban-V3-WilliamGu3/lib/conj.h \
+ /home/runner/KomplexKanban-V3-WilliamGu3/lib/multiplication.h
