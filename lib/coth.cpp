684481b9a0c5fa7b30f3f3300f290f652424e9eb
jacobpmathew <jpmaustin@gmail.com>
2023-12-11 15:39:13 +0000

    coth, tanh functions

---
+++ lib/coth.cpp
@@ -0,0 +1,10 @@
+#include "coth.h"
+#include "sinh.h"
+#include "cosh.h"
+#include "division.h"
+#include <cmath>
+
+Complex coth(const Complex z)
+{
+  return Complex(cosh(z.real)*cos(z.img),sinh(z.real)*sin(z.img))/Complex(sinh(z.real)*cos(z.img),cosh(z.real)*sin(z.img));
+}
